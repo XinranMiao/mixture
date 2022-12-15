553a309c305af90dd1c258524af1f347e26335a5
XinranMiao <69163928+XinranMiao@users.noreply.github.com>
2022-12-15 11:57:06 -0600

    r files

--- R/reshape.R
+++ R/reshape.R
@@ -6,7 +6,17 @@ count2prob <- function(y) {
 }
 
 post_rel_abund <- function(p_sim) {
-  p_sim%>%
+  p_sim %>%
+    reshape_onedraw() %>%
+    rename(sample = "row",
+           Taxon = "col") %>%
+    mutate(Taxon = paste0("V", Taxon)) %>%
+    select(value, sample,Taxon)
+}
+
+
+reshape_onedraw <- function(x) {
+  x %>%
     as.data.frame() %>%
     pivot_longer(everything()) %>%
     mutate(
@@ -14,9 +24,38 @@ post_rel_abund <- function(p_sim) {
       col = str_extract(name, ",[0-9]+"),
       row = as.integer(row),
       col = as.integer(str_remove(col, ","))
+    )
+}
+
+
+
+#' Extract quantiles of regression coefficients
+coef_quantile <- function(n, fit) {
+  fit$draws(n) %>%
+    apply(2, quantile, c(0.05, 0.5, 0.95)
     ) %>%
-    rename(sample = "row",
-           Taxon = "col") %>%
-    mutate(Taxon = paste0("V", Taxon)) %>%
-    select(value, sample,Taxon)
+    t() %>%
+    as.data.frame() %>%
+    mutate(name = n) %>%
+    rename(lwr = `5%`,
+           upr = `95%`,
+           estimate = `50%`) %>%
+    rowid_to_column(var = "Taxon") %>%
+    mutate(Taxon = str_extract(Taxon, "[0-9]+"),
+           Taxon = str_c("V", Taxon))
+}
+
+coef_prod_quantile <- function(n1, n2, fit) {
+  (fit$draws(n1) * fit$draws(n2)) %>%
+    apply(2, quantile, c(0.05, 0.5, 0.95)
+    ) %>%
+    t() %>%
+    as.data.frame() %>%
+    mutate(name = str_c(n1, ".", n2)) %>%
+    rename(lwr = `5%`,
+           upr = `95%`,
+           estimate = `50%`) %>%
+    rowid_to_column(var = "Taxon") %>%
+    mutate(Taxon = str_extract(Taxon, "[0-9]+"),
+           Taxon = str_c("V", Taxon))
 }
--- R/simulate.R
+++ R/simulate.R
@@ -1,17 +1,17 @@
-phi_inv <- function(z) {
-  p <- c(exp(z), 1)
+phi_inv <- function(mu) {
+  p <- c(exp(mu), 1)
   p / sum(p)
 }
 
 lnm_simulator <- function(X, beta, sigma = 1, depth = 3000) {
   N <- nrow(X)
   K <- nrow(beta)
-  Z <- rnorm(K, X %*% t(beta), sigma)
+  mu <- rnorm(K, X %*% t(beta), sigma)
   y <- matrix(nrow = N, ncol = K + 1)
   
   for (i in 1:N) {
-    z <- rnorm(K, X[i, ] %*% t(beta), sigma)
-    y[i, ] <- rmultinom(1, depth, phi_inv(z))
+    mu <- rnorm(K, X[i, ] %*% t(beta), sigma)
+    y[i, ] <- rmultinom(1, depth, phi_inv(mu))
   }
   y
 }
